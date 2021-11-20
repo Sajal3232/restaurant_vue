d0bd10342f6c3ce575641df1166ecfdaa91277f2
sajal3232 <sajalhossain3232@gmail.com>
2021-11-20 14:57:25 +0600

    part-12 logout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
+    <compHeader/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import compHeader from './components/Header.vue'
 export default {
-
 components : {
+  compHeader
 }
 
 }
@@ -20,6 +22,11 @@ components : {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+body{
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container">
+            
+            <div class="collapse navbar-collapse bg-info justify-content-around" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link"  href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Add Restuarant</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Edit Restuarant</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" v-on:click="logout" href="#">Logout</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+ 
+   </div>
+</template>
+
+<script>
+    export default{
+        name: "Header",
+
+        methods: {
+            logout(){
+                localStorage.clear();
+                this.$router.push({name:'Login'});
+            }
+        },
+    }
+</script>
